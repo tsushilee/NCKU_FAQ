8153f7e2dc9559531a05d5c1c9243c40f6314af1
Chester <j6fm06503@gmail.com>
2017-01-04 17:11:07 +0800

    heroku test

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "ㄏㄏ")
+                    send_message(sender_id, "我能為你做些什麼？")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
