f19ac313c10d3282be3d73c27c628ba0c676a731
Chester <j6fm06503@gmail.com>
2017-01-15 17:33:16 +0800

    postback

--- app.py
+++ app.py
@@ -53,19 +53,26 @@ def webhook():
                     pass
 
                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+                    ender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["postback"]["payload"]  # the message's text
+                    message_text = message_text.encode('utf-8').lower()
+                    reply = handle_message( message_text )
+                    send_message(sender_id, reply )
 
     return "ok", 200
 
 def handle_message(message_text):
     if u'信箱'.encode("utf8") in message_text or 'e-mail' in message_text or 'e mail' in message_text or 'email' in message_text :
         return '信箱問題'
+    if u'啟動'.encode("utf8") in message_text or u'啟用'.encode("utf8") in message_text or u'授權軟體'.encode("utf8") in message_text  or 'office' in message_text or 'visual studio' in message_text :
+        return '若您在學校以外的網路,啟用時必須先啟動vpn,才能進行產品認證'
+
     if u'成功入口'.encode("utf8") in message_text :
         print(editdistance.eval('成功入口忘記密碼 想要改密碼', message_text ))
         print(type(editdistance.eval('成功入口忘記密碼 想要改密碼', message_text )))
-        #if editdistance.eval('成功入口忘記密碼 想要改密碼', message_text ) < 3
-            #return '請攜帶雙證件(學生證以及身分證)於上班時間到計算機中心一樓服務台,做更改密碼之服務'
-        return '成功入口問題'
+        if u'改'.encode("utf8") in message_text :
+            return '若需要修改成功入口密碼,請攜帶雙證件(學生證以及身分證)於上班時間到計算機中心一樓服務台,做更改密碼之服務'
 
     return message_text
 
@@ -97,9 +104,14 @@ def send_message(recipient_id, message_text):
                         "title":"成功入口"
                         },
                         {
-                        "type":"postback",
-                        "title":"沒事",
-                        "payload":"USER_DEFINED_PAYLOAD"
+                        "type":"web_url",
+                        "url":"http://cc.ncku.edu.tw/files/11-1255-7637.php?Lang=zh-tw",
+                        "title":"SSL VPN服務"
+                        },
+                        {
+                            "type":"postback",
+                            "title":"授權軟體",
+                            "payload":"授權軟體"
                         }
                         ]
                 }
